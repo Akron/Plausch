77a6ae6eef049f0c2438d06a3fa9dd2a32e5f8b4
Akron <nils@diewald-online.de>
2015-02-07 17:38:56 +0100

    Some more specs

--- spec/plauschSpec.js
+++ spec/plauschSpec.js
@@ -1,6 +1,19 @@
-describe('Plausch', function () {
+describe('Plausch.Comment', function () {
   it('should be initializable', function () {
-    var plausch = Plausch.Comment.create();
+    var plausch = Plausch.Comment.create(undefined, "Super Text!");
     expect(plausch).toBeTruthy();
   });
+
+  it('should be stringifiable', function () {
+    var plausch = Plausch.Comment.create(undefined, "Super Text!");
+    expect(plausch.toString()).toEqual("Super Text!");
+  });
+
+  it('may have an author', function () {
+    var plausch = Plausch.Comment.create("Akron", "Super Text!");
+    expect(plausch.toString()).toEqual("Akron: Super Text!");
+  });
+
+  it('should have a date');
+  it('should support adding comments');
 });
--- src/plausch.js
+++ src/plausch.js
@@ -9,8 +9,16 @@ var Plausch = Plausch || {};
   };
 
   Plausch.Comment = {
-    create : function () {
-      return Object.create(Plausch.Comment);
+    create : function (author, text) {
+      return Object.create(Plausch.Comment)._init(author, text);
+    },
+    _init : function (author, text) {
+      this._author = author;
+      this._comment = text;
+      return this;
+    },
+    toString : function () {
+      return (this._author !== undefined ? this._author + ': ' : '')  + this._comment;
     }
   };
 }(this.Plausch));
