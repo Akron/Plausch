4071ca26ef68357e40e7c230df6d0f6fbb59a3ed
Akron <nils@diewald-online.de>
2015-02-08 17:52:13 +0100

    Updated spec and added element creation

--- spec/plauschSpec.js
+++ spec/plauschSpec.js
@@ -4,18 +4,127 @@ describe('Plausch.Comment', function () {
     expect(plausch).toBeTruthy();
   });
 
-  it('should be stringifiable', function () {
-    var plausch = Plausch.Comment.create(undefined, "Super Text!");
-    expect(plausch.toString()).toEqual("Super Text!");
-  });
-
   it('may have an author', function () {
     var plausch = Plausch.Comment.create("Akron", "Super Text!");
     expect(plausch.toString()).toEqual("Akron: Super Text!");
+    expect(plausch.author()).toEqual('Akron');
+    expect(plausch.toString()).toEqual("Akron: Super Text!");
+
+    plausch = Plausch.Comment.create(undefined, "Super neuer Text!");
+    expect(plausch.author()).toBeUndefined();
+    expect(plausch.toString()).toEqual("Super neuer Text!");
+    plausch.author("Julian");
+    expect(plausch.author()).toEqual('Julian');
+    expect(plausch.toString()).toEqual("Julian: Super neuer Text!");
   });
 
-  it('should have a date');
-  it('should support adding comments');
-  it('should be upvotable');
-  it('should be downvotable');
+  it('should have a text', function () {
+    var plausch = Plausch.Comment.create();
+    expect(plausch).toBeNull();
+
+    plausch = Plausch.Comment.create(undefined);
+    expect(plausch).toBeNull();
+
+    plausch = Plausch.Comment.create("test");
+    expect(plausch).toBeNull();
+
+    plausch = Plausch.Comment.create("test", null);
+    expect(plausch).toBeNull();
+
+    plausch = Plausch.Comment.create("test", undefined);
+    expect(plausch).toBeNull();
+
+
+    plausch = Plausch.Comment.create("test1", "Versuch1");
+    expect(plausch.author()).toEqual('test1');
+    expect(plausch.text()).toEqual('Versuch1');
+    expect(plausch.toString()).toEqual("test1: Versuch1");
+
+    plausch.author('test2')
+    plausch.text('Versuch2')
+    expect(plausch.author()).toEqual('test2');
+    expect(plausch.text()).toEqual('Versuch2');
+    expect(plausch.toString()).toEqual("test2: Versuch2");
+  });
+
+  it('should be stringifiable', function () {
+    var plausch = Plausch.Comment.create(undefined, "Super Text!");
+    expect(plausch.toString()).toEqual("Super Text!");
+
+    plausch = Plausch.Comment.create('Akron', "Super Text!");
+    expect(plausch.toString()).toEqual("Akron: Super Text!");
+  });
+
+  xit('should be submittable', function () {
+    var plausch = Plausch.Comment.create(undefined, "Super Text!");
+    expect(plausch).not.toBeNull();
+    expect(plausch.submitted).toBe(false);
+    expect(plausch.submit()).toBe(true);
+    expect(plausch.submitted).toBe(true);
+  });
+
+
+  it('should be represented by an HTML node', function () {
+    var plausch = Plausch.Comment.create(undefined, "Super Text!");
+    var e = plausch.element();
+    expect(e.getAttribute('class')).toEqual('plausch');
+    expect(e.firstChild.getAttribute('class')).toEqual('plausch-text');
+    expect(e.firstChild.firstChild.nodeValue).toEqual('Super Text!');
+
+    plausch = Plausch.Comment.create('Akron', "Besserer Text!");
+    e = plausch.element();
+    expect(e.getAttribute('class')).toEqual('plausch');
+
+    expect(e.firstChild.getAttribute('class')).toEqual('plausch-author');
+    expect(e.firstChild.firstChild.nodeValue).toEqual('Akron');
+
+    expect(e.lastChild.getAttribute('class')).toEqual('plausch-text');
+    expect(e.lastChild.firstChild.nodeValue).toEqual('Besserer Text!');
+  });
+
+
+  // In case the author or the text is changed,
+  // the HTML representation has to change
+  it('should update the HTML on attribute change', function () {
+    var plausch = Plausch.Comment.create('Patricia', "Toller Text!");
+    var e = plausch.element();
+    expect(e.firstChild.firstChild.nodeValue).toEqual('Patricia');
+    expect(e.lastChild.firstChild.nodeValue).toEqual('Toller Text!');
+
+    // Update text content
+    plausch.text('Super toller Text!');
+    expect(e.lastChild.firstChild.nodeValue).toEqual('Super toller Text!');
+
+    // Update author content
+    plausch.author('Patti');
+    expect(e.firstChild.firstChild.nodeValue).toEqual('Patti');
+  });
+
+
+  // The date of the comment will e retrieved from the
+  // server response - client wise it's undefined
+  it('should have have a date (if set by server)', function () {
+    var plausch = Plausch.Comment.create('Patricia', "Toller Text!");
+    expect(plausch.date('2005-11-25')).toBeUndefined();
+    expect(plausch.date()).toBeUndefined();
+  });
+
+  // Comments can be attached to paragraphs
+  it('could be attached to a paragraph');
+  // Comments may also be attached to text snippets
+  // marked by the comentator
+  it('could be attached to text snippets');
+
+  // Replies to the comment are represented by Plausch.Reply
+  it('should support adding replies');
+  it('should be serializable to JSON');
+
+  // Comments may be up or downvoted by other commentators,
+  // which may have an impact on the visibility.
+  // That means - all comments can be sorted by their relevance.
+  it('may be upvotable');
+  it('may be downvotable');
+  it('may support smileys in text');
 });
+
+xdescribe('Plausch.Reply');
--- src/plausch.js
+++ src/plausch.js
@@ -1,24 +1,113 @@
+/**
+ * Plausch is a sidenode comment system.
+ */
 var Plausch = Plausch || {};
 
 (function (Plausch) {
   "use strict";
 
-  // Default log message
+  // Default log message - should be overwritten
   Plausch.log = Plausch.log || function (type, msg) {
     console.log(type + ": " + msg);
   };
 
+  /**
+   * Represent a comment in a Plausch.
+   */
   Plausch.Comment = {
+    submitted : false,
+
+    // Create a new object
     create : function (author, text) {
-      return Object.create(Plausch.Comment)._init(author, text);
+      if (text !== undefined && text !== null)
+	return Object.create(Plausch.Comment)._init(author, text);
+      return null;
     },
+
+    // Initialize the object by values
     _init : function (author, text) {
       this._author = author;
-      this._comment = text;
+      this._text   = text;
       return this;
     },
+
+    // Get or set author
+    author : function (author) {
+      if (arguments.length === 1) {
+	this._author = author;
+	this.submitted = false;
+	this._update();
+      };
+      return this._author;
+    },
+
+    // Get or set text
+    text : function (text) {
+      if (arguments.length === 1) {
+	this._text = text;
+	this.submitted = false;
+	this._update();
+      };
+      return this._text;
+    },
+
+    // Get the date of the comment
+    date : function () {
+      // Date can only be set by the server
+      return this._date;
+    },
+
+    /* The HTML representation of the comment
+     * looks like ...
+     * <div class="plausch">
+     *   <span class="plausch-author">{author name}</span>
+     *   <span class="plausch-text">{comment text}</span>
+     * </div>
+     */
+    element : function () {
+      if (this._element !== undefined)
+	return this._element;
+
+      this._element = document.createElement('div');
+      this._element.setAttribute('class', 'plausch');
+
+      // Update element (Create author and text)
+      this._update();
+      return this._element;
+    },
+
+    // Update the represented HTML content
+    _update : function () {
+      if (this._element === undefined)
+	return;
+      
+      var comment = this._element;
+
+      // Remove all nodes (author or text) if set
+      while (comment.lastChild) {
+	comment.removeChild(this._element.lastChild);
+      };
+
+      // Create Author view
+      if (this._author != undefined) {
+	var author = document.createElement('span');
+	author.setAttribute('class', 'plausch-author');
+	author.appendChild(document.createTextNode(this._author));
+	comment.appendChild(author);
+      };
+
+      // Create Author view
+      var text = document.createElement('span');
+      text.setAttribute('class', 'plausch-text');
+      text.appendChild(document.createTextNode(this._text));
+      comment.appendChild(text);
+      this._element = comment;
+    },
+
+    // THis is more or less just for testing
     toString : function () {
-      return (this._author !== undefined ? this._author + ': ' : '')  + this._comment;
+      return (this._author !== undefined ? this._author + ': ' : '') +
+	this._text;
     }
   };
 }(this.Plausch));
